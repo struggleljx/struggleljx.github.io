fb923085fd92b6d11a4e97b438b1658e80878d3a
168yyy <517792647@qq.com>
2016-03-21 15:09:50 +0800

    ..

--- js/zhuye.js
+++ js/zhuye.js
@@ -33,13 +33,30 @@ $(function() {
 
 	var $neirong=$('.neirong');
 	$neirong.bind('swipeLeft',function(){
-		
 		if($(this).next().length){
 			$(this).css({display: 'none',opacity: 0});
 			$(this).next().css({display: 'block'}).animate({opacity: 1},600);
 		}else{
 			$(this).css({display: 'block'}).animate({opacity: 1},600);
 		}
+		      $(".c1").animate({
+				  height: "98%"
+			  },2000);
+			  $(".c2").animate({
+				  height: "98%"
+			  },2000)
+			  $(".c3").animate({
+				  height: "99%"
+			  },2000)
+			  $(".c4").animate({
+				  height: "99%"
+			  },2000)
+			  $(".c5").animate({
+				  height: "90%"
+			  },2000)
+			  $(".c6").animate({
+				  height: "90%"
+			  },2000)
 	})
 	$neirong.bind('swipeRight',function(){
 		if($(this).prev().length){
@@ -48,9 +65,51 @@ $(function() {
 		}else{
 			$(this).css({display: 'block'}).animate({opacity: 1},600);
 		}
+		      $(".c1").animate({
+				  height: "98%"
+			  },2000);
+			  $(".c2").animate({
+				  height: "98%"
+			  },2000)
+			  $(".c3").animate({
+				  height: "99%"
+			  },2000)
+			  $(".c4").animate({
+				  height: "99%"
+			  },2000)
+			  $(".c5").animate({
+				  height: "90%"
+			  },2000)
+			  $(".c6").animate({
+				  height: "90%"
+			  },2000)
 	})
 
 	$('.zhuye').click(function(){
 		$('.neirong').css({display: 'none'});
 	})
+
+	   
+	   $('.btn3').click(function(){
+	      $(".c1").animate({
+			  height: "98%"
+		  },2000);
+		  $(".c2").animate({
+			  height: "98%"
+		  },2000)
+		  $(".c3").animate({
+			  height: "99%"
+		  },2000)
+		  $(".c4").animate({
+			  height: "99%"
+		  },2000)
+		  $(".c5").animate({
+			  height: "90%"
+		  },2000)
+		  $(".c6").animate({
+			  height: "90%"
+		  },2000)
+
+	   })	
+	   
 })
